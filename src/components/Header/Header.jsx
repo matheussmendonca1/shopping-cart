1d89b779a19560f87fc39b2fdba511873a57853b
Matheus Mendonça <m.e.mendonca2007@gmail.com>
2024-01-06 13:02:01 -0300

    REACT - carrinho de compras digital finalizado

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import CartButton from '../CartButton/CartButton';
+
+import './Header.css';
+
+export default function Header() {
+	return(
+		<header className="header">
+			<div className="container">
+				<SearchBar />
+				<CartButton />
+			</div>
+		</header>
+	);
+}
---
+++ src/utils/formatCurrency.js
@@ -0,0 +1,8 @@
+const formatCurrency = (value, currency) => {
+	return value.toLocaleString('pt-BR', {
+		style: 'currency',
+		currency
+	});
+};
+
+export default formatCurrency;
